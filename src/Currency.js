5ec5aa86c000541f84f598b99386920b42fb17e9
Mintaka <chenyujie@192.168.0.154>
2022-10-06 13:10:26 +0800

    add arrow icon

--- src/Currency.js
+++ src/Currency.js
@@ -5,7 +5,7 @@ function Currency({amount}) {
     <div className="">
         <div className="px-1">BTC</div>
         <input className="border-2 border-gray-300 p-1 font-bold px-10"
-        type="number" value={amount}
+        type="number" 
         />
     </div>
   )
--- src/ExchangeRate.js
+++ src/ExchangeRate.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowsRotate} from '@fortawesome/free-solid-svg-icons'
 
 function ExchangeRate({ currency }) {
   return (
-    <div className="flex grey text-sm">
+    <div className="flex grey text-sm p-2">
         <div className="font-bold">Exchange Rate:</div>
         <div> 1 BTC = {currency} HKD</div>
-        
+        <div className='px-3'>
+          <FontAwesomeIcon icon={faArrowsRotate} className=" text-orange-500 text-xl" />
+        </div>
     </div>
   )
 }
