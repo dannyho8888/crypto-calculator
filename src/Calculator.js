47d7a08d71a602f9b4d65a982063790340da65af
Ho Ho Yin <yinllmar@gmail.com>
2022-10-06 19:49:48 +0800

    finish eventHandler, top/bottom amount change mutually but setState is kind of delay, i will fixed it later

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,40 +3,58 @@ import Currency from './Currency'
 import ExchangeRate from './ExchangeRate'
 
 function Calculator() {
-  const [topAmount, setTopAmount] = useState(0);
-  const [bottomAmount, setBottomAmount] = useState(10);
-  const [currCurrency, setCurrCurrency] = useState(15000);
+  const [topAmount, setTopAmount] = useState(1);
+  const [bottomAmount, setBottomAmount] = useState(1);
+  const [rate, setRate] = useState(150000);
 
-  const fetchCurrency = async () => {
-    try {
-      const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice/HKD.json")
-      const data = await res.json();
-      setCurrCurrency(data.bpi.HKD.rate_float);
-    } catch (e) {
-      console.log("Error when fetching data");
+  // update currency every 15 seconds
+  useEffect(() => {
+    const fetchCurrency = async () => {
+      try {
+        const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice/HKD.json")
+        const data = await res.json();
+        setRate(data.bpi.HKD.rate_float);
+      } catch (e) {
+        console.log("Error when fetching data");
+      } 
     }
+    const intervalId = setInterval(() => {
+      fetchCurrency();
+      console.log(rate);
+    }, 15000);
+    return () => clearInterval(intervalId);
+  }, [rate])
+
+  function handleTopAmountChange(e) {
+    setTopAmount(e.target.value);
+    setBottomAmount(topAmount * rate);
+  }
+  function handleBottomAmountChange(e) {
+    setBottomAmount(e.target.value);
+    setTopAmount(bottomAmount/rate);
   }
 
-  // update currency every 30 seconds
-  useEffect(() => {
-    const intervalId = setInterval(async () => {
-      fetchCurrency();
-      console.log(currCurrency);
-    }, 30000);
-    return () => clearInterval(intervalId);
-  }, [])
-
   return (
-    <div>
+    <div> 
         <div className="px-2 text-xl grey">
             Lite Cryptocurrency Exchange Calculator
         </div>
         
         <div className="bg-white cal-width rounded-xl px-8 py-10 mt-5">
-            <Currency amount={topAmount}/>
-            <ExchangeRate currency={currCurrency}/>
+            <Currency 
+              amount={topAmount} 
+              symbol="BTC" 
+              onChangeAmount={handleTopAmountChange}
+            />
+            
+            <ExchangeRate currency={rate}/>
             <hr />
-            <Currency amount={bottomAmount}/>
+
+            <Currency
+              amount={bottomAmount} 
+              symbol="HKD" 
+              onChangeAmount={handleBottomAmountChange}
+            />
         </div>
     </div>
     
--- src/Currency.js
+++ src/Currency.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function Currency({amount}) {
+function Currency({ symbol, amount, onChangeAmount }) {
   return (
-    <div className="">
-        <div className="px-1">BTC</div>
-        <input className="border-2 border-gray-300 p-1 font-bold px-10"
-        type="number" 
+    <div>
+        <div className="pl-2 font-semibold grey">{symbol}</div>
+        <input className="border-2 border-gray-300 py-1 pl-2 pr-12 font-medium text-2xl"
+          type="number" 
+          value={amount}
+          onChange={onChangeAmount}
         />
     </div>
   )
